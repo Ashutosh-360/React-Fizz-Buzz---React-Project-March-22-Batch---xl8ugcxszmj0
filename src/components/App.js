1019177a693d51aa01029d3642b111471a93ddb9
ASHUTOSH NAIK <108534953+Ashutosh-360@users.noreply.github.com>
2022-09-30 20:08:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+const [count,setCount]=useState(1);
 
-  
+
+const incrementHandler=()=>
+{
+setCount(count+1);
+}
+
+const decrementHandler=()=>{
+  setCount(count-1);
+}
+
   return (
     <div id="main">
-
+        <div className={(count%5==0 && count%3==0)?"fizzbuzz":(count%5==0)?"buzz":(count%3==0)?"fizz":"normal"} id="counter">{count}</div>
+        <button id="increment" onClick={incrementHandler}>increment</button>
+        <button id="decrement" onClick={decrementHandler}>decrement</button>
     </div>
   )
 }
-
-
 export default App;
